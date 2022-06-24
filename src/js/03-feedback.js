53a5feecca176e5c0941feda6dd5f8ee4b9eb906
Eldiwr <Eldiwr97@gmail.com>
2022-06-24 16:05:45 +0300

    feedback - done
    Решение 3-й задачи.

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,25 +1,27 @@
+import throttle from "lodash.throttle";
 
 const form = document.querySelector('.feedback-form');
 
 const formData = {};
 const TEXT_FORM = 'feedback-form-state';
 
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
 populateTextarea();
 
-form.addEventListener('submit', onFormSubmit);
-
-form.addEventListener('input', event => {
+function onFormInput(event){
     formData[event.target.name] = event.target.value;
 
     localStorage.setItem(TEXT_FORM, JSON.stringify(formData));
-});
+};
 
 function onFormSubmit(event) {
     event.preventDefault();
 
     event.currentTarget.reset();
     localStorage.removeItem(TEXT_FORM);
-}
+};
 
 function populateTextarea() {
     const savedMessage = JSON.parse(localStorage.getItem(TEXT_FORM));
@@ -27,5 +29,9 @@ function populateTextarea() {
     if (savedMessage) {
         form.email.value = savedMessage.email;
         form.message.value = savedMessage.message;
+    } else if (savedMessage === null) {
+        savedMessage.email = " ";
+        savedMessage.message = " ";
     }
-}
+};
+
