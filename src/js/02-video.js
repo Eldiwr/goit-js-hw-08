fe256678c7ee1a91013a36bd0ea3831f5dd78475
Eldiwr <Eldiwr97@gmail.com>
2022-06-25 20:44:09 +0300

    02 -video  - done
    Закончил вторую задачу.

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,12 @@
 import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 const CURRENT_TIME = "videoplayer-current-time";
 
-player.on('timeupdate', onPlay);
+player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay(data) {
     localStorage.setItem(CURRENT_TIME, data.seconds);
