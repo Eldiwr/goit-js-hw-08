c3f21bb8b5c1958964f629f7bd9f93ab6779014b
Eldiwr <Eldiwr97@gmail.com>
2022-06-25 15:26:23 +0300

    Vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const CURRENT_TIME = "videoplayer-current-time";
+
+player.on('timeupdate', onPlay);
+
+function onPlay(data) {
+    localStorage.setItem(CURRENT_TIME, data.seconds);
+};
+
+const currentTime = JSON.parse(localStorage.getItem(CURRENT_TIME));
+
+if (currentTime) {
+    player.setCurrentTime(currentTime);
+};
